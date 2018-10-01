b6fc4fe2980853b74ccd907adc0358231a6b27de
alec3243 <alec3243@gmail.com>
2018-09-30 22:46:46 -0700

    Lecture 3 code

---
+++ HW2/src/HelloRunnable.java
@@ -0,0 +1,13 @@
+public class HelloRunnable implements Runnable {
+
+	public static void main(String[] args) {
+		(new Thread(new HelloRunnable())).start();
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Hello from a thread!");
+
+	}
+
+}
---
+++ HW2/src/SleepMessages.java
@@ -0,0 +1,12 @@
+public class SleepMessages {
+
+	public static void main(String[] args) throws InterruptedException {
+		String importantInfo[] = { "Mares eat oats", "Does eat oats",
+				"Little lambs eat ivy", "A kid will eat ivy too" };
+		for (int i = 0; i < importantInfo.length; i++) {
+			Thread.sleep(1000);
+			System.out.println(importantInfo[i]);
+		}
+	}
+
+}
